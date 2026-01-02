d722690f7d69e148957013680a70d3d8f89c6904
rudydanila <rudydanila.ggpk@gmail.com>
2026-01-03 00:08:44 +0300

    Первый коммит. Создание проекта и структуры страницы входа

---
+++ app/src/main/java/com/rudydanila/codeshark/LoginActivity.kt
@@ -0,0 +1,220 @@
+package com.rudydanila.codeshark
+
+import android.R.attr.label
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.credentials.GetCredentialException
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.credentials.CredentialManager
+import androidx.credentials.GetCredentialRequest
+import androidx.credentials.exceptions.GetCredentialCancellationException
+import androidx.credentials.exceptions.GetCredentialCustomException
+import androidx.credentials.exceptions.NoCredentialException
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenParsingException
+import com.rudydanila.codeshark.ui.theme.CodeSharkTheme
+import kotlinx.coroutines.delay
+import java.security.SecureRandom
+import java.util.Base64
+
+
+class LoginActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //replace with your own web client ID from Google Cloud Console
+        val webClientId = "113583699572-crfodb6gqvjfntqm0h3q2br4sc4u1jpq.apps.googleusercontent.com"
+        setContent {
+            CodeSharkTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background,
+                ) {
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+
+                    ) {
+                        //This will trigger on launch
+                        BottomSheet(webClientId)
+
+                        //This requires the user to press the button
+                        LogInUI(webClientId)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+@Composable
+fun BottomSheet(webClientId: String) {
+    val context = LocalContext.current
+    LaunchedEffect(Unit) {
+        val googleIdOption: GetGoogleIdOption = GetGoogleIdOption.Builder()
+            .setFilterByAuthorizedAccounts(true)
+            .setServerClientId(webClientId)
+            .setNonce(generateSecureRandomNonce())
+            .build()
+        val request: GetCredentialRequest = GetCredentialRequest.Builder()
+            .addCredentialOption(googleIdOption)
+            .build()
+        val e = signIn(request, context)
+        if (e is NoCredentialException) {
+            val googleIdOptionFalse: GetGoogleIdOption = GetGoogleIdOption.Builder()
+                .setFilterByAuthorizedAccounts(false)
+                .setServerClientId(webClientId)
+                .setNonce(generateSecureRandomNonce())
+                .build()
+
+            val requestFalse: GetCredentialRequest = GetCredentialRequest.Builder()
+                .addCredentialOption(googleIdOptionFalse)
+                .build()
+            signIn(requestFalse, context)
+        }
+    }
+}
+
+
+@RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+@Composable
+fun LogInUI(webClientId: String) {
+    val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+    val onClick: () -> Unit = { /* ... ваша логика входа ... */ }
+
+    // 1. Объявляем ДВА отдельных состояния для полей Email и Password
+    var emailInput by remember { mutableStateOf("") }
+    var passwordInput by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 32.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // Image(TODO: Add App Logo)
+
+        OutlinedTextField(
+            value = emailInput,
+            onValueChange = { newValue: String -> emailInput = newValue },
+            label = { Text("Email") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = passwordInput,
+            onValueChange = { newValue: String -> passwordInput = newValue },
+            label = { Text("Password") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Button(onClick = {},){
+            Text ("Войти", fontSize = 16.sp)
+        }
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.siwg_button),
+            contentDescription = "Sign in with Google Button",
+            modifier = Modifier
+                .wrapContentSize(Alignment.Center)
+                .clickable(enabled = true, onClick = onClick)
+        )
+    }
+}
+
+fun generateSecureRandomNonce(byteLength: Int = 32): String {
+    val randomBytes = ByteArray(byteLength)
+    SecureRandom.getInstanceStrong().nextBytes(randomBytes)
+    return Base64.getUrlEncoder().withoutPadding().encodeToString(randomBytes)
+}
+
+@RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+suspend fun signIn(request: GetCredentialRequest, context: Context): Exception? {
+    val credentialManager = CredentialManager.create(context)
+    val failureMessage = "Sign in failed!"
+    val e: Exception? = null
+    delay(250)
+    try {
+        val result = credentialManager.getCredential(
+            request = request,
+            context = context,
+        )
+        Log.i(TAG, result.toString())
+
+        Toast.makeText(context, "Sign in successful!", Toast.LENGTH_SHORT).show()
+        Log.i(TAG, "(☞ﾟヮﾟ)☞  Sign in Successful!  ☜(ﾟヮﾟ☜)")
+
+    } catch (e: GetCredentialException) {
+        Toast.makeText(context, failureMessage, Toast.LENGTH_SHORT).show()
+        Log.e(TAG, "$failureMessage: Failure getting credentials", e)
+
+    } catch (e: GoogleIdTokenParsingException) {
+        Toast.makeText(context, failureMessage, Toast.LENGTH_SHORT).show()
+        Log.e(TAG, "$failureMessage: Issue with parsing received GoogleIdToken", e)
+
+    } catch (e: NoCredentialException) {
+        Toast.makeText(context, failureMessage, Toast.LENGTH_SHORT).show()
+        Log.e(TAG, "$failureMessage: No credentials found", e)
+        return e
+
+    } catch (e: GetCredentialCustomException) {
+        Toast.makeText(context, failureMessage, Toast.LENGTH_SHORT).show()
+        Log.e(TAG, "$failureMessage: Issue with custom credential request", e)
+
+    } catch (e: GetCredentialCancellationException) {
+        Toast.makeText(context, ": Sign-in cancelled", Toast.LENGTH_SHORT).show()
+        Log.e(TAG, "$failureMessage: Sign-in was cancelled", e)
+    }
+    return e
+}
+@RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+@Preview(showBackground = true)
+@Composable
+fun PreviewButtonUI() {
+    CodeSharkTheme {
+        LogInUI(webClientId = "dummy_client_id")
+    }
+}
